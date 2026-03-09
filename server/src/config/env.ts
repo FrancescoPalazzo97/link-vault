c41a3d6ba7a19f0b530e8b411446109d658189c4
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-09 13:40:13 +0100

    refactor: move envSchema definition to env.ts and remove env.model.ts

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,12 @@
-import { envSchema } from "../models/env.model.js";
+import z from "zod";
+
+export const envSchema = z.object({
+	PORT: z.string().default("3000"),
+	NODE_ENV: z.string().default("development"),
+	MONGO_URI: z
+		.string()
+		.default("mongodb://admin:secret@localhost:27017/linkvault?authSource=admin"),
+});
 
 const result = envSchema.safeParse(process.env);
 
--- server/src/models/env.model.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const envSchema = z.object({
-	PORT: z.string().default("3000"),
-	NODE_ENV: z.string().default("development"),
-	MONGO_URI: z
-		.string()
-		.default("mongodb://admin:secret@localhost:27017/linkvault?authSource=admin"),
-});
