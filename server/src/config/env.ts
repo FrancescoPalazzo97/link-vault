c9f9fe23bda1b30f16d90494836492f1e24b9b2e
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-09 14:45:14 +0100

    refactor: remove export from env schema

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,11 +1,13 @@
 import z from "zod";
 
-export const envSchema = z.object({
+const envSchema = z.object({
 	PORT: z.string().default("3000"),
 	NODE_ENV: z.string().default("development"),
 	MONGO_URI: z
 		.string()
 		.default("mongodb://admin:secret@localhost:27017/linkvault?authSource=admin"),
+	JWT_SECRET: z.string().min(1),
+	PASSWORD_HASH: z.string().min(1),
 });
 
 const result = envSchema.safeParse(process.env);
