8b92c9a446127899db6efce54b5f8405e3fa6f19
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-16 16:58:35 +0100

    feat: add logo component

---
+++ client/src/components/layout/Logo.tsx
@@ -0,0 +1,10 @@
+import { IconLink } from "@tabler/icons-react";
+
+export function Logo() {
+	return (
+		<>
+			<IconLink size={20} />
+			<span className="font-semibold text-sm">LinkVault</span>
+		</>
+	);
+}
