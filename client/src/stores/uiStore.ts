46fb5ffc84d75bf2117c20429f5403149ef8ee8a
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-16 16:56:31 +0100

    feat: update uiStore

--- client/src/stores/uiStore.ts
+++ client/src/stores/uiStore.ts
@@ -2,14 +2,19 @@ import { create } from "zustand";
 
 interface UiState {
 	isAddLinkOpen: boolean;
+	mobileOpen: boolean;
 	openAddLink: () => void;
 	closeAddLink: () => void;
+	setMobileOpen: (isOpen: boolean) => void;
 }
 
 export const useUiStore = create<UiState>()((set) => ({
 	isAddLinkOpen: false,
+	mobileOpen: false,
 
 	openAddLink: () => set({ isAddLinkOpen: true }),
 
 	closeAddLink: () => set({ isAddLinkOpen: false }),
+
+	setMobileOpen: (isOpen) => set({ mobileOpen: isOpen }),
 }));
